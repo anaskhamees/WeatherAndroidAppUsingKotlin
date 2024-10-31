2c2adf60894a5046a0c56357b2943fbe3f8fbcf2
anaskhamees <anaskhamees65@gmail.com>
2024-11-01 00:07:15 +0200

    model +network

---
+++ app/src/main/java/com/example/weatherforecast/Model/AlarmEntity.kt
@@ -0,0 +1,11 @@
+package com.example.weatherforecast.Model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "alarms")
+data class AlarmEntity(
+    @PrimaryKey(autoGenerate = true) // Add this
+    val alarmId: Int = 0, // Make alarmId the primary key
+    val timeInMillis: Long
+)
